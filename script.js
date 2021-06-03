ffd692451846cf1e6e238e7d79148e8cbd37fc25
Anton Kleimenov <akleimenov.exceedteam@gmail.com>
2021-06-03 10:47:34 +0300

    simple app v1

---
+++ script.js
@@ -0,0 +1,23 @@
+const options = document.querySelector('select');
+const btn = document.querySelector('.btn-save');
+const input = document.querySelector('input');
+
+document.addEventListener('DOMContentLoaded', () => {
+    input.value = options.children[0].innerHTML;
+});
+
+btn.addEventListener('click', () => {
+    const selected = options.selectedIndex;
+    input.value = input.value.trim();
+    if (input.value.length < 1) {
+        input.style.borderColor = 'red';
+    } else {
+        input.style.borderColor = 'green';
+        options[selected].innerHTML = input.value;
+    }
+});
+
+options.addEventListener('change', () => {
+    const selected = options.selectedIndex;
+    input.value = options[selected].innerHTML;
+});
